0d77a872fd126fdea3d781714f805565861b3562
Alex Kolchinski <y.a.kolchinski@gmail.com>
2014-03-19 10:50:10 -0500

    Updated neural-net-on-synthetic-data discussion

--- synth_features.py
+++ synth_features.py
@@ -6,24 +6,23 @@ numTest = 1000  #Number of test points per class
 P_HIGH = .4
 P_LOW = .1
 P_BGD = .2 #background pixel probability
-N = 100 #length of vectors
-M = 10 #number of high/low probability pixels
 
-
-def posVector():
+def posVector(N = 100, M = 10):
   highPart = random(M) < P_HIGH
   lowPart = random(M) < P_LOW
   bgPart = random(N - 2*M) < P_BGD
   return 1 * np.append(np.append(highPart, lowPart), bgPart)
 
-def negVector():
+def negVector(N = 100, M = 10):
   return 1 * (random(N) < P_BGD)
 
-def synthData(ptsPerClass):
+# M is length of vectors, N is number of high/low (as opposed to background)
+# probability pixels
+def synthData(ptsPerClass, N = 100, M = 10):
   data = np.zeros((2 * ptsPerClass, N));
   for i in range(ptsPerClass):
-    data[i] = negVector()
-    data[ptsPerClass + i] = posVector()
+    data[i] = negVector(N, M)
+    data[ptsPerClass + i] = posVector(N, M)
   return data
     
 
